fe57dbaa7c00bdda30952e1de1121e6e591867c4
catherine <vuthi.esther@gmail.com>
2022-06-09 20:05:23 +0300

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+
+    #instance Method goes here
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+end
+
+p1 = Person.new
+p1.talk
+p1.walk
